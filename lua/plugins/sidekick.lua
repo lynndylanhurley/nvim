a62234c9b5aedfce05dfe57163a24a399fc307df
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2025-10-17 18:57:05 -0500

    sidekick.nvim mappings

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -24,42 +24,65 @@ return {
     {
       "<c-.>",
       function()
-        require("sidekick.cli").focus()
+        require("sidekick.cli").toggle()
       end,
-      desc = "Sidekick Switch Focus",
-      mode = { "n", "v" },
+      desc = "Sidekick Toggle",
+      mode = { "n", "t", "i", "x" },
     },
     {
       "<leader>aa",
       function()
-        require("sidekick.cli").toggle({ focus = true })
+        require("sidekick.cli").toggle()
       end,
       desc = "Sidekick Toggle CLI",
-      mode = { "n", "v" },
     },
+    {
+      "<leader>as",
+      function()
+        require("sidekick.cli").select()
+      end,
+      -- Or to select only installed tools:
+      -- require("sidekick.cli").select({ filter = { installed = true } })
+      desc = "Select CLI",
+    },
+    {
+      "<leader>at",
+      function()
+        require("sidekick.cli").send({ msg = "{this}" })
+      end,
+      mode = { "x", "n" },
+      desc = "Send This",
+    },
+    {
+      "<leader>af",
+      function()
+        require("sidekick.cli").send({ msg = "{file}" })
+      end,
+      desc = "Send File",
+    },
+    {
+      "<leader>av",
+      function()
+        require("sidekick.cli").send({ msg = "{selection}" })
+      end,
+      mode = { "x" },
+      desc = "Send Visual Selection",
+    },
+    {
+      "<leader>ap",
+      function()
+        require("sidekick.cli").prompt()
+      end,
+      mode = { "n", "x" },
+      desc = "Sidekick Select Prompt",
+    },
+    -- Example of a keybinding to open Claude directly
     {
       "<leader>ac",
       function()
         require("sidekick.cli").toggle({ name = "claude", focus = true })
       end,
-      desc = "Sidekick Claude Toggle",
-      mode = { "n", "v" },
-    },
-    {
-      "<leader>ag",
-      function()
-        require("sidekick.cli").toggle({ name = "grok", focus = true })
-      end,
-      desc = "Sidekick Grok Toggle",
-      mode = { "n", "v" },
-    },
-    {
-      "<leader>ap",
-      function()
-        require("sidekick.cli").select_prompt()
-      end,
-      desc = "Sidekick Ask Prompt",
-      mode = { "n", "v" },
+      desc = "Sidekick Toggle Claude",
     },
   },
 }
